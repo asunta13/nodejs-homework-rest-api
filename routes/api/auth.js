30b9f9a241526e441fda91d843d0fa906fe18500
Anastasiya Pekarik <anastasiya.pekarik@gmail.com>
2023-04-14 14:41:48 +0300

    ready

---
+++ routes/api/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/auth");
+
+const router = express.Router();
+const { validateBody } = require("../../utils");
+const { authenticate } = require("../../middlewares");
+const { schemas } = require("../../models/user");
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.get("/current", authenticate, ctrl.getCurrent);
+router.post("/logout", authenticate, ctrl.logout);
+router.patch(
+	"/",
+	authenticate,
+	validateBody(schemas.subscriptionSchema),
+	ctrl.updateSubscription
+);
+
+module.exports = router;
