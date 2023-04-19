4fa079bcbcf3c984249d85b0f3a88bc838a7654f
Anastasiya Pekarik <anastasiya.pekarik@gmail.com>
2023-04-19 14:09:20 +0300

    part1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const fs = require("fs/promises");
+const path = require("path");
+const Jimp = require("jimp");
 
 const { ctrlWrapper } = require("../utils");
 const { User } = require("../models/user");
@@ -8,6 +12,8 @@ const { HttpError } = require("../helpers");
 
 const { SECRET_KEY } = process.env;
 
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
+
 const register = async (req, res) => {
 	const { email, password } = req.body;
 	const user = await User.findOne({ email });
@@ -15,7 +21,12 @@ const register = async (req, res) => {
 		throw HttpError(409, "Email in use");
 	}
 	const hashPassword = await bcrypt.hash(password, 10);
-	const result = await User.create({ ...req.body, password: hashPassword });
+	const avatarURL = gravatar.url(email);
+	const result = await User.create({
+		...req.body,
+		password: hashPassword,
+		avatarURL,
+	});
 	res.status(201).json({
 		email: result.email,
 		subscription: result.subscription,
@@ -73,10 +84,30 @@ const updateSubscription = async (req, res) => {
 	res.status(200).json({ email, subscription });
 };
 
+const updateAvatar = async (req, res) => {
+	const { _id } = req.user;
+	const { path: tempUpload, filename } = req.file;
+	await Jimp.read(`${tempUpload}`)
+		.then((image) => {
+			return image.resize(250, 250).writeAsync(`${tempUpload}`);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+
+	const avatarName = `${_id}_${filename}`;
+	const resultUpload = path.join(avatarsDir, avatarName);
+	await fs.rename(tempUpload, resultUpload);
+	const avatarURL = path.join("avatars", avatarName);
+	await User.findByIdAndUpdate(_id, { avatarURL });
+	res.json({ avatarURL });
+};
+
 module.exports = {
 	register: ctrlWrapper(register),
 	login: ctrlWrapper(login),
 	getCurrent: ctrlWrapper(getCurrent),
 	logout: ctrlWrapper(logout),
 	updateSubscription: ctrlWrapper(updateSubscription),
+	updateAvatar: ctrlWrapper(updateAvatar),
 };
--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new Schema({
 		enum: subscriptionList,
 		default: "starter",
 	},
+	avatarURL: {
+		type: String,
+		required: true,
+	},
 	token: String,
 });
 
